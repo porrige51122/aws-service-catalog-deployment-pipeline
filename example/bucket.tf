ba5567ff0715b74f7234758cb3421c380945e918
porrige51122 <aidan_smithers@btinternet.com>
2022-10-02 14:37:22 +0100

    Remove random variable and use variable instead

--- example/bucket.tf
+++ example/bucket.tf
@@ -1,12 +1,12 @@
 data "archive_file" "template" {
   type        = "zip"
-  source_file = "./cloudformation/example-ec2.template"
-  output_path = "./cloudformation/example-ec2.zip"
+  source_file = "${path.module}/cloudformation/example-ec2.template"
+  output_path = "${path.module}/cloudformation/example-ec2.zip"
 }
 
 resource "aws_s3_object" "template" {
   bucket = aws_s3_bucket.template_store.bucket
-  key    = "example-ec2.zip"
+  key    = basename(data.archive_file.template.output_path)
   source = data.archive_file.template.output_path
 
   etag = filemd5(data.archive_file.template.output_path)
