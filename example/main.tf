1d7b4e0725d16755c3937b9c51d6463b9c48f5ed
porrige51122 <aidan_smithers@btinternet.com>
2022-10-02 17:03:39 +0100

    Move files into different structure

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "service_catalog_deployment_pipeline" {
-  source               = "../"
+  source               = "../module/"
   template_bucket_name = aws_s3_bucket.template_store.bucket
   template_bucket_arn  = aws_s3_bucket.template_store.arn
   template_key         = aws_s3_object.template.key
