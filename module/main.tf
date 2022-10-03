d48fb588235812b0e14f3ec2c30ee9a9fd324c4a
Aidan Smithers <aidan_smithers@btinternet.com>
2022-10-03 12:39:00 +0100

    Add Manual approval stage with manual approval comments and url (#12)
    * Add Manual approval stage with manual approval comments and url
    
    * Change manual approval url to correct url
    
    * Remove bucket_domain_name var as it isn't used anymore

--- module/main.tf
+++ module/main.tf
@@ -1,5 +1,9 @@
 data "aws_caller_identity" "current" {}
 
 locals {
+  # Common Resource Name so name can be changed centrally much easier
   common_resource_name = "sc-pipeline-${data.aws_caller_identity.current.account_id}"
+
+  # Automatically fill manual_approval_url with S3 Bucket URL
+  manual_approval_url = var.manual_approval_url == "" ? "https://s3.console.aws.amazon.com/s3/buckets/${var.template_bucket.bucket}" : var.manual_approval_url
 }
--- module/variables.tf
+++ module/variables.tf
@@ -6,13 +6,15 @@ variable "template_bucket" {
   })
 }
 
-variable "template_key" {
-  description = "Key to the zipped template object in S3"
-  type        = string
+variable "template_zip_object" {
+  description = "Zipped template object in S3"
+  type = object({
+    key = string
+  })
 
   validation {
-    condition     = endswith(var.template_key, ".zip")
-    error_message = "The template_key value must be a compressed file ending with \".zip\"."
+    condition     = endswith(var.template_zip_object.key, ".zip")
+    error_message = "The template_zip_object.key value must be a compressed file ending with \".zip\"."
   }
 }
 
@@ -32,4 +34,16 @@ variable "service_catalog_product" {
     arn = string
     id  = string
   })
+}
+
+variable "manual_approval_comments" {
+  description = "The comments displayed to the user when manual approval is needed"
+  type        = string
+  default     = "A review is needed for deploying this service catalog product"
+}
+
+variable "manual_approval_url" {
+  description = "The url you want to provide to the user as part of the approval request"
+  type        = string
+  default     = ""
 }
