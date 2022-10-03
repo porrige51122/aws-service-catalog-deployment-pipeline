40b6dd5019a5f48d30d7c14f19bd5b22fc6e996e
porrige51122 <aidan_smithers@btinternet.com>
2022-10-03 12:28:44 +0100

    Change manual approval url to correct url

--- module/main.tf
+++ module/main.tf
@@ -5,5 +5,5 @@ locals {
   common_resource_name = "sc-pipeline-${data.aws_caller_identity.current.account_id}"
 
   # Automatically fill manual_approval_url with S3 Bucket URL
-  manual_approval_url = var.manual_approval_url == "" ? "https://terraform-20221003110118004900000002.s3.eu-west-1.amazonaws.com/example-ec2.zip" : var.manual_approval_url
+  manual_approval_url = var.manual_approval_url == "" ? "https://s3.console.aws.amazon.com/s3/buckets/${var.template_bucket.bucket}" : var.manual_approval_url
 }
