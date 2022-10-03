43c1294c7d61adc5ad7a5484729702c454abbcf4
porrige51122 <aidan_smithers@btinternet.com>
2022-10-03 17:16:47 +0100

    More comments added

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,21 @@
 module "service_catalog_deployment_pipeline" {
-  source                  = "../module/"
-  template_bucket         = aws_s3_bucket.template_store
-  template_zip_object     = aws_s3_object.template
-  template_path           = basename(data.archive_file.template.source_file)
+  # Source is relative for testing, externally use: 
+  # "github.com/porrige51122/aws-service-catalog-deployment-pipeline/module"
+  source = "../module/"
+
+  # These refer to details about the bucket where the template is stored
+  template_bucket     = aws_s3_bucket.template_store
+  template_zip_object = aws_s3_object.template
+  template_path       = basename(data.archive_file.template.source_file)
+
+  # This is the service catalog product that we are deploying to
   service_catalog_product = aws_servicecatalog_product.template
+
+  # This is an optional extra step in the pipeline that allows manual approval
   manual_approval_enabled = true
-  custom_test_buildspec   = data.local_file.buildspec_file.content
+  # manual_approval_comments = "Comments for the user to see on the approval stage"
+  # manual_approval_url      = "URL for the user to see during the approval stage"
+
+  # This is a custom buildspec that you can pass in, allowing for custom tests in your pipeline
+  custom_test_buildspec = data.local_file.buildspec_file.content
 }
